a8ec4d216e24d4811465b47310e3a9758b67fdcb
祝玲云 <1902733517@qq.com>
2020-10-13 17:09:01 +0800

    first commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface IHelloProps {
+    message?: string
+}
+
+// const Hello = (props: IHelloProps) => {
+//     return <h2>{props.message}</h2>
+// }
+
+// const Hello: React.FunctionComponent<IHelloProps> = (props) => {
+//     return <h2>{props.message}</h2>
+// }
+
+const Hello: React.FC<IHelloProps> = (props) => {
+    return <h2>{props.message}</h2>
+}
+
+Hello.defaultProps = {
+    message: 'Hello World'
+}
+
+export default Hello
